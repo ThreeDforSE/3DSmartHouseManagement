b20287302985456848be0b42da37c05c416ab0be
LiuFeng <emmelinev@163.com>
2016-06-30 20:02:22 +0800

    更新内容：1.主界面侧边栏主体完成，发现问题请及时反馈。2.js、css文件及内容更新。下一步解决：1.摄像机控制2.3D模型点击事件

--- ThreeDSmartHouse/WebContent/js/webGL.js
+++ ThreeDSmartHouse/WebContent/js/webGL.js
@@ -19,47 +19,13 @@
         document.addEventListener('mousedown', onDocumentMouseDown, false);
         document.addEventListener('mousemove', onDocumentMouseMove, false);
 
-        // create the ground plane
-        var floorTex = THREE.ImageUtils.loadTexture("images/floor-wood.jpg");
-        var plane = new THREE.Mesh(new THREE.BoxGeometry(60, 60, 1, 1), new THREE.MeshPhongMaterial({
-            color: 0xffffff,
-            map: floorTex
-        }));
-
-        // rotate and position the plane
-        plane.rotation.x = -0.5 * Math.PI;
-        plane.position.x = 15;
-        plane.position.y = 0;
-        plane.position.z = 0;
-
-        // add the plane to the scene
-        scene.add(plane);
-
-
-        // create a cube
-        var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
-        var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
-        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-
-
-        // position the cube
-        cube.position.x = -9;
-        cube.position.y = 3;
-        cube.position.z = 0;
-
-        // add the cube to the scene
-        scene.add(cube);
-
-        var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
-        var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
-        var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-        // position the sphere
-        sphere.position.x = 20;
-        sphere.position.y = 0;
-        sphere.position.z = 2;
-        // add the sphere to the scene
-        scene.add(sphere);
+        
+        initPlane(scene);
+        initGrid(scene);
+        initCube(scene);
+        initSphere(scene); 
+        initRefrig(scene); 
+        setCamera(camera,scene);
 
         // var cylinderGeometry = new THREE.CylinderGeometry(2, 2, 20);
         // var cylinderMaterial = new THREE.MeshLambertMaterial({color: 0x77ff77});
@@ -68,13 +34,6 @@
         // cylinder.position.set(0, 0, 1);
 
         // scene.add(cylinder);
-
-
-        // position and point the camera to the center of the scene
-        camera.position.x = -30;
-        camera.position.y = 40;
-        camera.position.z = 30;
-        camera.lookAt(scene.position);
 
         // add subtle ambient lighting
         var ambientLight = new THREE.AmbientLight(0x0c0c0c);
@@ -108,26 +67,27 @@
             if (tube) scene.remove(tube)
         });
 
+       
         render();
 
         function render() {
         //    stats.update();
             // rotate the cube around its axes
-            cube.rotation.x += controls.rotationSpeed;
-            cube.rotation.y += controls.rotationSpeed;
-            cube.rotation.z += controls.rotationSpeed;
+            // cube.rotation.x += controls.rotationSpeed;
+            // cube.rotation.y += controls.rotationSpeed;
+            // cube.rotation.z += controls.rotationSpeed;
 
-            // bounce the sphere up and down
-            step += controls.bouncingSpeed;
-            sphere.position.x = 20 + ( 10 * (Math.cos(step)));
-            sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(step)));
+            // // bounce the sphere up and down
+            // step += controls.bouncingSpeed;
+            // sphere.position.x = 20 + ( 10 * (Math.cos(step)));
+            // sphere.position.y = 2 + ( 10 * Math.abs(Math.sin(step)));
 
-            // scale the cylinder
+            // // scale the cylinder
 
-            scalingStep += controls.scalingSpeed;
-            var scaleX = Math.abs(Math.sin(scalingStep / 4));
-            var scaleY = Math.abs(Math.cos(scalingStep / 5));
-            var scaleZ = Math.abs(Math.sin(scalingStep / 7));
+            // scalingStep += controls.scalingSpeed;
+            // var scaleX = Math.abs(Math.sin(scalingStep / 4));
+            // var scaleY = Math.abs(Math.cos(scalingStep / 5));
+            // var scaleZ = Math.abs(Math.sin(scalingStep / 7));
             // cylinder.scale.set(scaleX, scaleY, scaleZ);
 
             // render using requestAnimationFrame
@@ -197,26 +157,86 @@
             }
         }
 
-        // function initStats() {
+   
+    }
+     function initGrid(scene){
+        var helper = new THREE.GridHelper( 1000, 50 );
+        helper.setColors( 0x0000ff, 0x808080 );
+        scene.add( helper );
+    }
 
-        //     var stats = new Stats();
+    function initPlane(scene){
+        // create the ground plane
+        var floorTex = THREE.ImageUtils.loadTexture("images/floor-wood.jpg");
+        var plane = new THREE.Mesh(new THREE.BoxGeometry(60, 60, 1, 1), new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: floorTex
+        }));
 
-        //     stats.setMode(0); // 0: fps, 1: ms
+        // rotate and position the plane
+        plane.rotation.x = -0.5 * Math.PI;
+        plane.position.x = 15;
+        plane.position.y = 0;
+        plane.position.z = 0;
 
-        //     // Align top-left
-        //     stats.domElement.style.position = 'absolute';
-        //     stats.domElement.style.left = '0px';
-        //     stats.domElement.style.top = '0px';
+        // add the plane to the scene
+        scene.add(plane);
+    }
 
-        //     // document.getElementById("Stats-output").appendChild(stats.domElement);
+    function initCube(scene){
+        // create a cube
+        var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+        var cubeMaterial = new THREE.MeshLambertMaterial({color: 0xff0000});
+        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
 
-        //     return stats;
-        // }
 
-        function initGrid(){
-				var helper = new THREE.GridHelper( 1000, 50 );
-				helper.setColors( 0x0000ff, 0x808080 );
-				scene.add( helper );
-			}
+        // position the cube
+        cube.position.x = -9;
+        cube.position.y = 3;
+        cube.position.z = 0;
+
+        // add the cube to the scene
+        scene.add(cube);
+    }
+
+    function initRefrig(scene){
+        // create a cube
+        var surfaceTex = THREE.ImageUtils.loadTexture("images/floor-wood.jpg");
+        var cubeGeometry = new THREE.BoxGeometry(4, 4, 8);
+        var cubeMaterial = new THREE.MeshPhongMaterial({
+            color: 0xffffff,
+            map: surfaceTex
+        });
+        var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+
+
+        // position the cube
+        cube.position.x = -1;
+        cube.position.y = 3;
+        cube.position.z = 0;
+
+        // add the cube to the scene
+        scene.add(cube);
+    }
+
+    function initSphere(scene){
+        var sphereGeometry = new THREE.SphereGeometry(4, 20, 20);
+        var sphereMaterial = new THREE.MeshLambertMaterial({color: 0x7777ff});
+        var sphere = new THREE.Mesh(sphereGeometry, sphereMaterial);
+
+        // position the sphere
+        sphere.position.x = 20;
+        sphere.position.y = 0;
+        sphere.position.z = 2;
+        // add the sphere to the scene
+        scene.add(sphere);
+    }
+
+    function setCamera(camera,scene){
+        // position and point the camera to the center of the scene
+        camera.position.x = -30;
+        camera.position.y = 40;
+        camera.position.z = 30;
+        camera.lookAt(scene.position);
     }
     window.onload = init;
